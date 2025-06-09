e94e739762d71352fccf67f960f06b07a0d2941c
Liam Morales <dredknives04@gmail.com>
2025-06-09 15:22:20 +0800

    Added fetchloanaction file

---
+++ Web/Stores/Loans/Actions/FetchLoanAction.cs
@@ -0,0 +1,40 @@
+﻿using LoanManagement.Core.Models; 
+
+namespace LoanManagement.Web.Stores.Loans.Actions;
+
+public class FetchLoansAction
+{
+    public string? SearchTerm { get; }
+
+    public FetchLoansAction(string? searchTerm = null)
+    {
+        SearchTerm = searchTerm;
+    }
+}
+
+public class FetchLoansSuccessAction
+{
+    private IEnumerable<Core.Models.Loan> loans;
+
+    public IEnumerable<Loan> Loans { get; }
+
+    public FetchLoansSuccessAction(IEnumerable<Loan> loans)
+    {
+        Loans = loans;
+    }
+
+    public FetchLoansSuccessAction(IEnumerable<Core.Models.Loan> loans)
+    {
+        this.loans = loans;
+    }
+}
+
+public class FetchLoansFailureAction
+{
+    public string ErrorMessage { get; }
+
+    public FetchLoansFailureAction(string errorMessage)
+    {
+        ErrorMessage = errorMessage;
+    }
+}
