45bc7d9d5d45ddbae96a82de2f8d3f23f2429357
Liam Morales <dredknives04@gmail.com>
2025-06-09 15:22:42 +0800

    Added loan state file

---
+++ Web/Stores/Loans/State/LoanState.cs
@@ -0,0 +1,31 @@
+﻿using Fluxor;
+
+using LoanManagement.Core.Models;
+
+namespace LoanManagement.Web.Stores.Loans.State;
+
+[FeatureState]
+public class LoanState
+{
+    public bool IsLoading { get; } = false;
+    public IEnumerable<Loan> Loans { get; } = Enumerable.Empty<Loan>();
+    public Loan? SelectedLoan { get; } = null;
+    public string? ErrorMessage { get; } = null;
+    public string SearchTerm { get; } = string.Empty;
+
+    private LoanState() { } // Required for creating initial state
+    
+    public LoanState(
+        bool isLoading, 
+        IEnumerable<Loan> loans, 
+        Loan? selectedLoan, 
+        string? errorMessage,
+        string searchTerm)
+    {
+        IsLoading = isLoading;
+        Loans = loans;
+        SelectedLoan = selectedLoan;
+        ErrorMessage = errorMessage;
+        SearchTerm = searchTerm;
+    }
+}
