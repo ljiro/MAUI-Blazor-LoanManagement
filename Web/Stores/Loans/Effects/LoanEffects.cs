72de800a1b51c911a28853fdb5f822e1e41d5bee
Liam Morales <dredknives04@gmail.com>
2025-06-09 15:22:27 +0800

    Added loan effect file

---
+++ Web/Stores/Loans/Effects/LoanEffects.cs
@@ -0,0 +1,29 @@
+﻿using LoanManagement.Services;
+using LoanManagement.Web.Stores.Loans.Actions;
+using Fluxor;
+using IDispatcher = Fluxor.IDispatcher;
+namespace LoanManagement.Web.Stores.Loan.Effects;
+
+public class LoanEffects
+{
+    private readonly ILoanService _loanService;
+
+    public LoanEffects(ILoanService loanService)
+    {
+        _loanService = loanService;
+    }
+
+    [EffectMethod]
+    public async Task HandleFetchLoans(FetchLoansAction action, IDispatcher dispatcher)
+    {
+        try
+        {
+            var loans = await _loanService.GetAllLoansAsync(action.SearchTerm);
+            dispatcher.Dispatch(new FetchLoansSuccessAction(loans));
+        }
+        catch (Exception ex)
+        {
+            dispatcher.Dispatch(new FetchLoansFailureAction(ex.Message));
+        }
+    }
+}
